00d44b464faa6375dd0dec105d153a344b3e2b75
JoshHalvorson <joshhalvorson16@gmail.com>
2020-04-25 12:33:00 -0700

    Add basic activity to test room join
    Join functionality not yet implemented, waiting for service impl

--- app/src/main/java/com/waveformhealth/MainActivity.kt
+++ app/src/main/java/com/waveformhealth/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.waveformhealth
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.waveformhealth.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    companion object {
+        const val TAG = "MainActivityLog"
+    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun joinRoom(view: View) {
+        Log.i(TAG, "join room button pressed")
     }
 }
