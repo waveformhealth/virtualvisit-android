51dc03c212a18470919e7e0adcd66221e2cde4c1
JoshHalvorson <joshhalvorson16@gmail.com>
2020-04-25 20:39:03 -0700

    Change token response

--- app/src/main/java/com/waveformhealth/model/ServiceTokenResponse.kt
+++ app/src/main/java/com/waveformhealth/model/ServiceTokenResponse.kt
@@ -1,12 +1,5 @@
 package com.waveformhealth.model
 
 data class ServiceTokenResponse(
-    val accountSid: String,
-    val algorithm: String,
-    val expiration: String,
-    val grants: ServiceGrant,
-    val id : String,
-    val identity: String,
-    val issuer: String,
-    val secretKey: ServiceSecretKey
+    val token: String
 )
