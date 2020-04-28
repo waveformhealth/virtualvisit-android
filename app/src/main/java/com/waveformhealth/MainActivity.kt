c7cd961e2c1360a875be5aa24e6b3959b382deb8
JoshHalvorson <joshhalvorson16@gmail.com>
2020-04-28 15:57:12 -0700

    Add permission checking before joining room

--- app/src/main/java/com/waveformhealth/MainActivity.kt
+++ app/src/main/java/com/waveformhealth/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.waveformhealth
 
+import android.Manifest
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.waveformhealth.databinding.ActivityMainBinding
 import com.waveformhealth.repo.WaveformServiceRepository
 import com.waveformhealth.room.RoomFragment
@@ -13,6 +19,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import okhttp3.Credentials
 import javax.inject.Inject
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,36 +39,60 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         (applicationContext as WaveformHealthApp).appComp().inject(this)
+
+        checkPermissions()
+    }
+
+    private fun checkPermissions(): Boolean {
+        var allGranted = false
+        Dexter.withContext(this)
+            .withPermissions(
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
+            ).withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    report?.let {
+                        allGranted = it.areAllPermissionsGranted()
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(permissions: List<PermissionRequest>?, token: PermissionToken?) {
+                    token?.continuePermissionRequest()
+                }
+            }).check()
+        return allGranted
     }
 
     fun joinRoom(view: View) {
         Log.i(TAG, "join room button pressed")
-
-        GlobalScope.launch {
-            withContext(Dispatchers.IO) {
-                val passCodeEncoded = Credentials.basic(BuildConfig.API_SECRET, "")
-                val roomResponse = waveformServiceRepository.createRoom(passCodeEncoded)
-                roomResponse?.let { serviceRoom ->
-                    roomSid = serviceRoom.sid
-                    Log.i(TAG, roomSid)
-                    val roomIdEncoded = Credentials.basic(serviceRoom.sid, "")
-                    val tokenResponse = waveformServiceRepository.requestToken(passCodeEncoded, roomIdEncoded)
-                    tokenResponse?.let { serviceTokenResponse ->
-                        accessToken = serviceTokenResponse.token
+        val granted = checkPermissions()
+        if (granted) {
+            GlobalScope.launch {
+                withContext(Dispatchers.IO) {
+                    val passCodeEncoded = Credentials.basic(BuildConfig.API_SECRET, "")
+                    val roomResponse = waveformServiceRepository.createRoom(passCodeEncoded)
+                    roomResponse?.let { serviceRoom ->
+                        roomSid = serviceRoom.sid
+                        Log.i(TAG, roomSid)
+                        val roomIdEncoded = Credentials.basic(serviceRoom.sid, "")
+                        val tokenResponse = waveformServiceRepository.requestToken(passCodeEncoded, roomIdEncoded)
+                        tokenResponse?.let { serviceTokenResponse ->
+                            accessToken = serviceTokenResponse.token
+                        }
                     }
                 }
-            }
 
-            withContext(Dispatchers.Main) {
-                val bundle = Bundle()
-                bundle.putString("accessToken", accessToken)
-                bundle.putString("roomSid", roomSid)
+                withContext(Dispatchers.Main) {
+                    val bundle = Bundle()
+                    bundle.putString("accessToken", accessToken)
+                    bundle.putString("roomSid", roomSid)
 
-                val roomFragment = RoomFragment()
-                roomFragment.arguments = bundle
+                    val roomFragment = RoomFragment()
+                    roomFragment.arguments = bundle
 
-                supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, roomFragment).commit()
-                toggleViews(visible = false)
+                    supportFragmentManager.beginTransaction().replace(R.id.fragmentContainer, roomFragment).commit()
+                    toggleViews(visible = false)
+                }
             }
         }
     }
